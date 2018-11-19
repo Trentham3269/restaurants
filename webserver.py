f168033b4242f2f3b4e84910cccb17c50309f1a2
Trentham3269 <scottdalwood@gmail.com>
2018-11-19 12:36:28 +0000

    Add post method to create resource

--- webserver.py
+++ webserver.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Restaurant, Base, MenuItem
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
 
 
 # Create db session
@@ -21,6 +22,11 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.end_headers()
                 output = ''
                 output += '<html><body>'
+                output += '''
+                    <a href='/restaurants/new'>
+                    <h3>Create a new restaurant</h3>
+                    </a>
+                    '''
                 for restaurant in restaurants:
                     output += restaurant.name
                     output += '</br>'
@@ -32,8 +38,46 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output.encode())
                 return
 
+            if self.path.endswith('/restaurants/new'):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ''
+                output += '<html><body>'
+                output += '''
+                    <h3>Create a new restaurant</h3>
+                    <form method="POST" action="/restaurants">
+                    <textarea name="message"></textarea>
+                    <br>
+                    <button type="submit">Create</button>
+                    </form>'''
+                output += '</body></html>'
+                self.wfile.write(output.encode())
+                return
+
         except IOError:
             self.send_error(404, 'File not found {}'.format(self.path))
+
+    def do_POST(self):
+        try:
+            # Parse form input
+            length = int(self.headers.get('Content-length', 0))
+            data = self.rfile.read(length).decode()
+            message = parse_qs(data)["message"][0]
+            message = message.replace("<", "&lt;")
+            # Insert row in db
+            new_restaurant = Restaurant(name=message)
+            session.add(new_restaurant)
+            session.commit()
+            # Redirect to restaurants page
+            self.send_response(303)
+            self.send_header('Content-type', 'text/html')
+            self.send_header('Location', '/restaurants')
+            self.end_headers()
+            return
+
+        except IOError:
+            pass
 
 
 def main():
