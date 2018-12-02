03e61267b7594801d2399fd151ab6ab728707c25
Trentham3269 <scottdalwood@gmail.com>
2018-12-02 05:09:04 +0000

    Add state token

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 from flask import Flask
 from flask import render_template, request, redirect, url_for, flash, jsonify
+from flask import session as login_session
+import random
+import string
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Restaurant, Base, MenuItem
@@ -17,6 +20,16 @@ engine = create_engine('sqlite:///restaurantmenu.db',
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+
+# Create a state token to prevent request forgery
+# Store it in the session for later verification
+@app.route('/login/')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.
+                    digits) for x in range(32))
+    login_session['state'] = state
+    return 'The current session state is {}'.format(login_session['state'])
 
 
 @app.route('/')
